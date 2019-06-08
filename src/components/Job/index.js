7bfc2dafc18b08dcd7012ad8419fcc9d482b1507
vanderkilu <vanderkiliku@gmail.com>
2019-06-08 13:29:03 -0400

    creating the job component markup

---
+++ src/components/Job/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Job() {
+    return (
+        <div className="job">
+            <h3 className="job__company">Google</h3>
+            <p className="job__role">Frontend Engineer</p>
+            <img src="" className="job__image" alt="job-image"/>
+            <p className="job__type">Full Time</p>
+            <p className="job__time"> 2days ago </p>
+        </div>
+    )
+}
