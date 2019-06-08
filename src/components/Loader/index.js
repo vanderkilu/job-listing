5c144f59091e1cbc200a620600ac89b5c2f7dcca
vanderkilu <vanderkiliku@gmail.com>
2019-06-08 14:10:58 -0400

    creating a loader component

---
+++ src/components/Loader/index.js
@@ -0,0 +1,13 @@
+import React from 'react' 
+
+function Loader() {
+    return (
+        <div class="balls">
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+    )
+}
+
+export default Loader
