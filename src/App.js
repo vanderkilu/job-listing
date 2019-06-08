ccce6747d94558666ce1b4c2c8f3e95140d60f19
vanderkilu <vanderkiliku@gmail.com>
2019-06-08 14:03:02 -0400

    refactoring job style

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import Welcome from './components/Welcome'
 import Search from './components/Search'
+import JobList from './components/JobList'
 
 function App() {
   return (
     <div className="wrapper">
         <Welcome />
         <Search/>
+        <JobList/>
     </div>
   );
 }
--- src/components/Job/index.js
+++ src/components/Job/index.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import './index.css'
 
 function Job() {
     return (
         <div className="job">
             <h3 className="job__company">Google</h3>
             <p className="job__role">Frontend Engineer</p>
-            <img src="" className="job__image" alt="job-image"/>
+            <img src="" className="job__image" alt="company icon"/>
             <p className="job__type">Full Time</p>
             <p className="job__time"> 2days ago </p>
         </div>
