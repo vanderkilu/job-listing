a0c3db5486513e7945aa005598d1327196399ec8
vanderkilu <vanderkiliku@gmail.com>
2019-06-09 14:00:53 -0400

    formating date to timesince

--- src/components/Job/index.js
+++ src/components/Job/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import './index.css'
+import { formatTime } from '../../utils'
 
 function Job({job}) {
     return (
@@ -8,7 +9,7 @@ function Job({job}) {
             <p className="job__role">{job.title}</p>
             <img src={job.company_logo} className="job__image" alt="company icon"/>
             <p className="job__type">{job.type}</p>
-            <p className="job__time"> 2days ago </p>
+            <p className="job__time"> {formatTime(new Date(job.created_at)) }</p>
         </div>
     )
 }
